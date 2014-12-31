a43039a84743fc4bb0b00ee2f75ebc46cecc388e
Ana Makarevich <electric.blake@gmail.com>
2015-01-01 02:05:29 +0300

    Initial Commit

---
+++ Matrices/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrices
+//
+//  Created by Anastasia Makarevich on 01/01/2015.
+//  Copyright (c) 2015 Anastasia Makarevich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
