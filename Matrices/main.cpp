7a1595b657bce4b46d1f124b819a6f6e3a75a0b1
Ana Makarevich <electric.blake@gmail.com>
2015-01-03 03:30:11 +0300

    Replaced bool arrays with structs

--- Matrices/main.cpp
+++ Matrices/main.cpp
@@ -7,17 +7,25 @@
 #include <iostream>
 
 using namespace std;
-void print_vector(bool* A, int);
-void add_zheg(bool* A, bool* B, bool* C, int);
-void add_slc(bool* A, bool* m, int);
-void get_vector(bool* vector, int size);
-void get_vector2(bool* vector, string thestring, int size);
+
+struct vector{
+    string name;
+    bool bits[8];
+};
+
+void print_vector(vector* A, int);
+void add_zheg(vector* A, vector* B, vector* C, int);
+void add_slc(vector* A, vector* m, int);
+void get_vector(vector* vector, int size);
+void get_vector2(vector* vector, string thestring, int size);
+
+
 int main(int argc, const char * argv[]) {
-    bool* vector;
-    vector = new bool[8];
+    vector* vect = new vector;
+    
     //get_vector(vector,8);
-    get_vector2(vector, "10101010", 8);
-    print_vector(vector,8);
+    get_vector2(vect, "a10101010", 8);
+    print_vector(vect,8);
     /*
      Test case for the example given on page 14
     bool A[8] = {1, 0, 1, 0, 0, 1, 0, 1};
@@ -37,45 +45,60 @@ int main(int argc, const char * argv[]) {
 }
 
 //First version that allows the user to enter the vector manually
-void get_vector(bool* vector, int size){
+void get_vector(vector* vector, int size){
     char keeper;
     for (int i = 0; i < size; i++) {
         cin >> keeper;
-        vector[i] = keeper=='1';
+        vector->bits[i] = keeper=='1';
     }
 }
 
 // get_vector2 converts the string into bool array: thus it would be easy to process the vectors once we've extracted them from the file
-void get_vector2(bool* vector, string thestring, int size) {
+void get_vector2(vector* vector, string thestring, int size) {
+    vector->name=thestring[0];
     for (int i = 0; i < size; i++) {
-        vector[i] = thestring[i]=='1';
+        vector->bits[i] = thestring[i+1]=='1';
     }
 }
 
 //add_scl - performs simple xor operation on vectors A and m and shifts all 1's to the left
-void add_slc(bool* A, bool* m, int size) {
+void add_slc(vector* A, vector* m, int size) {
     int count=0;
     for (int i = 0; i < size; i++){
-        if (A[i]^m[i]){
-            A[count]=1;
+        if (A->bits[i]^m->bits[i]){
+            A->bits[count]=1;
             count++;
         }
     }
     for (int i = count; i < size; i++) {
-        A[i]=0;
+        A->bits[i]=0;
     }
 }
 //add_zheg - pefroms bitwise and operation on vectors A and B and then performs xor operation on the resulting vector and B
-void add_zheg(bool* A, bool* B, bool* C, int size){
+void add_zheg(vector* A, vector* B, vector* C, int size){
     for (int i = 0; i < size; i++) {
-        C[i]=(A[i]&B[i])^B[i];
+        C->bits[i]=(A->bits[i]&B->bits[i])^B->bits[i];
     }
 }
+
+
 // print_vector simply prints out the given array, it's used to check if the program works properly at each step
-void print_vector(bool* A, int size) {
+void print_vector(vector* A, int size) {
+    cout << A->name;
     for (int i = 0; i < size; i++) {
-        cout << A[i];
+        cout << A->bits[i];
     }
     cout << endl;
+}
+
+void find_min(vector** vectors, vector* query, int num_vectors, int size){
+    vector* v1;
+    vector* v2;
+    for (int i = 0; i < num_vectors-1; i++) {
+        add_slc(vectors[i], query,size);
+        add_slc(vectors[i+1], query,size);
+        add_zheg(vectors[i],vectors[i+1], v1, size);
+        add_zheg(vectors[i+1], vectors[i], v2, size);
+    }
 }
 
